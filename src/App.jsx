dde97367f90151bac51ccf71d8ed2763119cd87d
ren132710 <ren132710@gmail.com>
2023-02-01 17:49:18 -0500

    API_KEY

--- src/App.jsx
+++ src/App.jsx
@@ -3,10 +3,10 @@ import { useExternalScript } from './useExternalScript';
 import './App.css';
 
 export default function App() {
-  const GOOGLE_API_KEY = import.meta.env.VITE_GOOGLE_API_KEY;
+  const API_KEY = import.meta.env.VITE_API_KEY;
 
   // https://developers.google.com/maps/documentation/javascript/url-params#required_parameters
-  const url = `https://maps.googleapis.com/maps/api/js?key=${GOOGLE_API_KEY}&libraries=places`;
+  const url = `https://maps.googleapis.com/maps/api/js?key=${API_KEY}&libraries=places`;
   const state = useExternalScript(url);
 
   return (
